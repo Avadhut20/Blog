06dbd080993fee37945afc9a06d483146f11e992
Avadhut20 <111897568+Avadhut20@users.noreply.github.com>
2024-01-03 17:50:01 +0530

    Added Authentication

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const path  =require("path");
 const userRoute=require('./routes/user')
+const blogRoute=require("./routes/blog")
 const express= require('express');
 const mongoose=require("mongoose");
+const cookie_parser=require("cookie-parser");
+const cookieParser = require("cookie-parser");
+const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 const app = express();
 const PORT=8000;
 mongoose.connect("mongodb://localhost:27017/blogify360")
 app.set("view engine","ejs");
 app.set("views",path.resolve("./views"));
 app.use(express.urlencoded({extended:false}));
-
+app.use(cookieParser())
+app.use(checkForAuthenticationCookie('token'))
 app.get('/',(req,res)=>{
-    res.render("home")
+    res.render("home",{
+        user:req.user
+    })
 })
 app.use("/user",userRoute)
+app.use("/blog",blogRoute)
 
 app.listen(PORT,()=>{
     console.log(`Server started at port:${PORT}`)
--- models/user.js
+++ models/user.js
@@ -1,57 +1,76 @@
-const {Schema,model}= require('mongoose');
-const { error } = require('node:console');
-const {randomBytes, createHmac}=require('node:crypto')
-const userSchema= new Schema({
-    fullName:{
-        type:String,
-        required:true,
+const { createHmac, randomBytes } = require("crypto");
+const { Schema, model } = require("mongoose");
+const { createTokenForUser } = require("../services/authentication");
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    salt:{
-        type:String,
-        
+    salt: {
+      type: String,
     },
-    password:{
-        type:String,
-        required:true,
+    password: {
+      type: String,
+      required: true,
     },
-    profileImageUrl:{
-        type:String,
-        default:"/images/default.png",
+    profileImageURL: {
+      type: String,
+      default: "/images/default.png",
     },
-    role:{
-        type:String,
-        enum:["USER","ADMIN"],
-        default:"USER",
-    }
-},
-{timestamps:true}
-)
-userSchema.pre('save',function (next){
-    const user=this;
-    if(!user.isModified('password')) return;
-    const salt=randomBytes(16).toString();
-    const hashedPassword=createHmac('sha256',salt)
-            .update(user.password)
-            .digest('hex');
-    this.salt=salt;
-    this.password=hashedPassword;
-    next();
-})
-userSchema.static('matchPassword',async function(email,password){
-    const user=await this.findOne({email});
-    if(!user) throw new Error('User not found!');
-    const salt=randomBytes(16).toString();
-    const hashedPassword=user.password;
-    const userProvidedHash=createHmac("sha256",salt)
-    .update(password)
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) return;
+
+  const salt = randomBytes(16).toString();
+  const hashedPassword = createHmac("sha256", salt)
+    .update(user.password)
     .digest("hex");
-    if(hashedPassword!==userProvidedHash) throw new Error("Incorrect Password");
-    return user;
-})
-const User = model('User',userSchema);
-module.exports=User;
+
+  this.salt = salt;
+  this.password = hashedPassword;
+
+  next();
+});
+
+userSchema.static(
+  "matchPasswordAndGenerateToken",
+  async function (email, password) {
+    const user = await this.findOne({ email });
+    if (!user) throw new Error("User not found!");
+
+    const salt = user.salt;
+    const hashedPassword = user.password;
+
+    const userProvidedHash = createHmac("sha256", salt)
+      .update(password)
+      .digest("hex");
+
+    if (hashedPassword !== userProvidedHash)
+      throw new Error("Incorrect Password");
+
+    const token = createTokenForUser(user);
+    const fullName=user.fullName;
+    return {token,fullName};
+  }
+);
+
+const User = model("user", userSchema);
+
+module.exports = User;
